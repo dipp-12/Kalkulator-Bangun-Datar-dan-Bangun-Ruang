3eb397e429168f2a5393e68f684a73bc095e7b8b
Nadhif Nashrullah <dipp@Nadhifs-MacBook-Pro.local>
2022-01-16 21:23:25 +0700

    add dictionary type2

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ output = document['output']
 selectType = document['select-type']
 selectCalculated = document['select-calculated']
 
+# Dictionary Bangun Datar
 type1 = {'Belah Ketupat': {'Keliling': {'formula': lambda sisi, x,y,z: 4 * sisi, 'input1': 'Masukkan Sisi', 'input2': 'Kosongkan', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}, 
                             'Luas': {'formula': lambda d1, d2, x,y: (1/2) * d1 * d2, 'input1': 'Masukkan Diameter 1', 'input2': 'Masukkan Diameter 2', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}},
         'Jajar Genjang': {'Keliling': {'formula': lambda a, b, x,y: 2 * (a + b), 'input1': 'Masukkan Sisi a', 'input2': 'Masukkan Sisi b', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}, 
@@ -27,11 +28,17 @@ type1 = {'Belah Ketupat': {'Keliling': {'formula': lambda sisi, x,y,z: 4 * sisi,
         'Trapesium': {'Keliling': {'formula': lambda a, b , c, d: a + b + c + d, 'input1': 'Masukkan Sisi a', 'input2': 'Masukkan Sisi b', 'input3': 'Masukkan Sisi c', 'input4': 'Masukkan Sisi d'}, 
                     'Luas': {'formula': lambda a, b, t,y: (1/2) * (a + b) * t, 'input1': 'Masukkan Sisi a', 'input2': 'Masukkan Sisi b', 'input3': 'Masukkan Tinggi', 'input4': 'Kosongkan'}}}
 
-type2 = {'Balok': {},
-        'Bola': {},
-        'Kerucut': {},
-        'Kubus': {},
-        'Tabung': {}}
+# Dictionary Bangun Ruang
+type2 = {'Balok': {'Luas Permukaan': {'formula': lambda p, l, t, x: 2 * (p * l + p * t + l * t), 'input1': 'Masukkan Panjang', 'input2': 'Masukkan Lebar', 'input3': 'Masukkan Tinggi', 'input4': 'Kosongkan'}, 
+                    'Volume': {'formula': lambda p, l, t, x: p * l * t, 'input1': 'Masukkan Panjang', 'input2': 'Masukkan Lebar', 'input3': 'Masukkan Tinggi', 'input4': 'Kosongkan'}},
+        'Bola': {'Luas Permukaan': {'formula': lambda r, x,y,z: 4 * math.pi * (r ** 2), 'input1': 'Masukkan Jari-Jari', 'input2': 'Kosongkan', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}, 
+                    'Volume': {'formula': lambda r, x,y,z: (4/3) * math.pi * (r ** 3), 'input1': 'Masukkan Jari-Jari', 'input2': 'Kosongkan', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}},
+        'Kerucut': {'Luas Permukaan': {'formula': lambda r, s ,x,y: math.pi * r * (r + s), 'input1': 'Masukkan Jari-Jari', 'input2': 'Masukkan Garis Pelukis', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}, 
+                    'Volume': {'formula': lambda r, t ,x,y: (1/3) *  math.pi * (r ** 2) * t, 'input1': 'Masukkan Jari-Jari', 'input2': 'Masukkan Tinggi', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}},
+        'Kubus': {'Keliling': {'formula': lambda sisi, x,y,z: 6 * (sisi ** 2), 'input1': 'Masukkan Sisi', 'input2': 'Kosongkan', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}, 
+                            'Luas': {'formula': lambda sisi, x,y,z: (sisi ** 3), 'input1': 'Masukkan Sisi', 'input2': 'Kosongkan', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}},
+        'Tabung': {'Luas Permukaan': {'formula': lambda r, t ,x,y: 2 * math.pi * r * (r + t), 'input1': 'Masukkan Jari-Jari', 'input2': 'Masukkan Tinggi', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}, 
+                    'Volume': {'formula': lambda r, t ,x,y: math.pi * (r ** 2) * t, 'input1': 'Masukkan Jari-Jari', 'input2': 'Masukkan Tinggi', 'input3': 'Kosongkan', 'input4': 'Kosongkan'}}}
 
 def selectTypeAction(x):
     x = selectType.value
@@ -44,8 +51,7 @@ def selectTypeAction(x):
             document['select-luas'].disabled = False
             document['select-luas-permukaan'].disabled = True
             document['select-volume'].disabled = True
-            
-
+                       
     for key in type2.keys():
         if key.find(x) > -1:
             console.log(key.find(x))
@@ -53,6 +59,7 @@ def selectTypeAction(x):
             document['select-luas'].disabled = True
             document['select-luas-permukaan'].disabled = False
             document['select-volume'].disabled = False
+
 
 def selectCalculatedAction(X):
     x = selectType.value
@@ -71,6 +78,7 @@ def selectCalculatedAction(X):
             input2.placeholder = type2[x][y]['input2']
             input3.placeholder = type2[x][y]['input3']
             input4.placeholder = type2[x][y]['input4']
+
 
 def getNum(x):
     temp = x
@@ -92,6 +100,10 @@ def formula(x, num1, num2, num3, num4):
     for key in type1.keys():
         if key.find(x) > -1:
             return type1[x][y]['formula'](num1, num2, num3, num4)
+
+    for key in type2.keys():
+        if key.find(x) > -1:
+            return type2[x][y]['formula'](num1, num2, num3, num4)
     
 
 def main(ev):
